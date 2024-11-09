cf3049caa506f9aeb702b62f858b3debd968aff9
AtharvaKamble <atharvadkamble00@gmail.com>
2024-11-09 12:20:09 -0500

    analytics: add google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,9 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "athk's blogs",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: { provider: 'google', tagId: 'G-Z5N5CRN31C' },
     locale: "en-US",
     baseUrl: "athk.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
