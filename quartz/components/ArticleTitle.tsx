d2a3c42f8d5f1aeef84c1e6e7acf14b8062d73eb
AtharvaKamble <atharvadkamble00@gmail.com>
2024-12-25 14:58:32 -0500

    add character-delayed title rendering

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,8 +3,9 @@ import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
+
   if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+    return <h1 id="cstm-title" class={classNames(displayClass, "article-title")}>{title}</h1>
   } else {
     return null
   }
@@ -13,7 +14,26 @@ const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzCompone
 ArticleTitle.css = `
 .article-title {
   margin: 2rem 0 0 0;
+  font-size: 4rem;
 }
+`
+
+ArticleTitle.afterDOMLoaded = `
+document.addEventListener("nav", () => {
+  const title = document.getElementById('cstm-title')?.innerHTML || "";
+  let toBeDisplayed = "";
+
+  let i = 0;
+  const interval = setInterval(() => {
+    if (i < title.length) {
+      toBeDisplayed += title[i++];
+      document.getElementById('cstm-title').innerHTML = toBeDisplayed;
+    } else {
+      clearInterval(interval);
+    }
+  }, 40);
+});
+
 `
 
 export default (() => ArticleTitle) satisfies QuartzComponentConstructor
